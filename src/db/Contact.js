fb07aaa946b956d96a80d7413352f2c79c709d57
DaryPet <d7akkord@gmail.com>
2024-06-25 14:56:45 +0200

    mongoosevalidation

--- src/db/Contact.js
+++ src/db/Contact.js
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { mongooseSaveError, setUpdateSettings } from './hooks.js';
 
 const contactsSchema = new Schema(
   {
@@ -31,6 +32,12 @@ const contactsSchema = new Schema(
     versionKey: false,
   },
 );
+
+contactsSchema.post('save', mongooseSaveError);
+
+contactsSchema.pre('findOneAndUpdate', setUpdateSettings);
+
+contactsSchema.post('findOneAndUpdate', mongooseSaveError);
 
 const Contact = model('contact', contactsSchema);
 
