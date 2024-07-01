a2a5d7f65c1f1e62e26a96c8c127ded99e0cd168
DaryPet <d7akkord@gmail.com>
2024-07-01 21:04:35 +0200

    UserShema

---
+++ src/db/User.js
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose';
+import { mongooseSaveError, setUpdateSettings } from './hooks.js';
+
+const usersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+usersSchema.post('save', mongooseSaveError);
+
+usersSchema.pre('findOneAndUpdate', setUpdateSettings);
+
+usersSchema.post('findOneAndUpdate', mongooseSaveError);
+
+const User = model('contact', usersSchema);
+
+export default User;
