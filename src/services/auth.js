2d1b556ed41aadf6a4cc23cf4254eeaaad011979
DaryPet <d7akkord@gmail.com>
2024-07-02 23:01:48 +0200

    hashedPassword

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,6 +4,8 @@ import bcrypt from 'bcrypt';
 export const findUser = (filter) => User.findOne(filter);
 
 export const signUpUser = async (payload) => {
-  const user = await User.create(payload);
+  const { password } = payload;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ ...payload, password: hashedPassword });
   return user;
 };
