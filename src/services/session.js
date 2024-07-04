4845b0f2024c826846b1fc8967f7d5b7462338a8
DaryPet <d7akkord@gmail.com>
2024-07-04 11:36:38 +0200

    deleteOldsession

--- src/services/session.js
+++ src/services/session.js
@@ -4,7 +4,9 @@ import { randomBytes } from 'crypto';
 // import createHttpError from 'http-errors';
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/indexSort.js';
 
-export const createSession = (userId) => {
+export const createSession = async (userId) => {
+  await SessionsCollection.deleteOne({ userId });
+
   const accessToken = randomBytes(30).toString('base64');
   const refreshToken = randomBytes(30).toString('base64');
   const accessTokenValidUntil = new Date(Date.now() + FIFTEEN_MINUTES);
