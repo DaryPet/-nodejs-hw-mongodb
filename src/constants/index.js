5fc5ba5249897319d2f5de40583dfdc6432972b5
DaryPet <d7akkord@gmail.com>
2024-07-10 14:56:47 +0200

    env+email

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
 export const SMTP = {
-  SMTP_HOST: 'smtp-relay.brevo.com',
-  SMTP_PORT: '587',
-  SMTP_USER: '7845d2001@smtp-brevo.com',
-  SMTP_PASSWORD:
-    'xsmtpsib-22a39b9542dbb2f712248607374ffce8667159537ce517ccb91328de072713cb-WZ3n6OqMUB9V1RCz',
-  SMTP_FROM: '7845d2001@smtp-brevo.com',
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
 };
